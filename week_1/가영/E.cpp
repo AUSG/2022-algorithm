8b428882d9f3fc2734326d9cc065c3a3ebb9e154
Kayoung <rkdkdudjd@gmail.com>
2022-09-02 17:43:43 +0900

    쿼리 마다 반복문 돌리지 않고 미리 계산해서 저장하기

--- week_1/가영/E.cpp
+++ week_1/가영/E.cpp
@@ -20,7 +20,7 @@ int main() {
 }
 
 void solve() {
-    int count[1000 + 1][1000 + 1] = {}; // count[i][j] : 가로 i, 세로 j인 rectangle 의 개수
+    int area[1000 + 1][1000 + 1] = {};
 
     int n, q; // n: the number of rectangles, q: the number of queries
     cin >> n >> q;
@@ -28,24 +28,23 @@ void solve() {
     for (int i = 0; i < n; i++) {
         int h, w;
         cin >> h >> w;
-        count[h][w]++;
+        area[h][w] += h * w;
     }
 
     int dp[1000 + 1][1000 + 1] = {};
 
-    for (int i = 0; i < q; i++) {
-        int hs, ws, hb, wb;
-        cin >> hs >> ws >> hb >> wb;
-
-        for (int x = 2; x <= hb; x++) {
-            if (dp[x][2] == 0) dp[x][2] = dp[x - 1][2] + (x - 1) * count[x - 1][1];
-            for (int y = 3; y <= wb; y++) {
-                if (dp[x][y] == 0) dp[x][y] = max(dp[x - 1][y], dp[x][y - 1]) + (x - 1) * (y - 1) * count[x - 1][y - 1];
+    for (long long x = 2; x <= hb; x++) {
+        if (dp[x][2] == 0) dp[x][2] = dp[x - 1][2] + area[x - 1][1];
+        for (long long y = 3; y <= wb; y++) {
+            if (dp[x][y] == 0) {
+                dp[x][y] = dp[x - 1][y] + dp[x][y - 1] - dp[x - 1][y - 1] + area[x - 1][y - 1];
             }
         }
+    }
 
-        // hb wb 보다 작은 애들 - hs ws 포함 애들 넓이 (중복 차감은 다시 더함)
+    for (int i = 0; i < q; i++) {
+        long long hs, ws, hb, wb;
+        cin >> hs >> ws >> hb >> wb;
         cout << dp[hb][wb] - dp[hs + 1][wb] - dp[hb][ws + 1] + dp[hs + 1][ws + 1] << endl;
-        // 모루겠따.
     }
 }
